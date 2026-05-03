d02c91d6cdbf46166274256b0f42f85ed72cc338
tranghuyen1409-eng <tranghuyen1409@gmail.com>
2026-05-03 17:10:32 +0700

    Update Chilli.java

--- Chilli.java
+++ Chilli.java
@@ -2,6 +2,7 @@ public class Chilli extends Fruit{
     public Chilli(int x, int y, String name){
         super(x, y, "Chilli");
     }
+    
     @Override
     public void onCollected(PacMan player){
                 player.setHasThorns(true); 
