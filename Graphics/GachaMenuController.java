daca9b6c5de8edcd73b0b2efee4b11709b41238b
pchauityu <pchau.ityu@gmail.com>
2026-05-04 00:02:21 +0700

    feat: gachamenu, skinmenu + mấy cái nhỏ nhỏ

--- Graphics/GachaMenuController.java
+++ Graphics/GachaMenuController.java
@@ -3,10 +3,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
-public class MenuController implements ActionListener{
+public class GachaMenuController implements ActionListener{
     private CardLayout cardLayout;
     private JPanel mainContainer;
-    public MenuController( CardLayout cardLayout, JPanel mainContainer){
+    public GachaMenuController( CardLayout cardLayout, JPanel mainContainer){
         this.cardLayout = cardLayout;
         this.mainContainer = mainContainer;
     }
@@ -14,21 +14,22 @@ public class MenuController implements ActionListener{
     public void actionPerformed(ActionEvent click) {
         String command = click.getActionCommand();
         switch (command){
-            case "Start":
-                cardLayout.show(mainContainer, "MapMenu");
+            case "1Roll":
+                cardLayout.show(mainContainer, "1 Roll");
                 break;
-            case "Gacha":
-                cardLayout.show(mainContainer, "GachaMenu");
+            case "10Rolls":
+                cardLayout.show(mainContainer, "10 Rolls");
                 break;
-            case "Skin":
-                cardLayout.show(mainContainer, "SkinMenu");
+            case "100Rolls":
+                cardLayout.show(mainContainer, "100 Rolls");
                 break;
-            case "Quit":
-                System.exit(0);
+            case "Back":
+                cardLayout.show(mainContainer, "MainMenu");
                 break;
             default:
                 System.out.println("chua set tinh nang cho nut nay");
                 break;
         }
     }
+
 }
