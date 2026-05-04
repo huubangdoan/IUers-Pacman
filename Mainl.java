14ca21189832d87cc71a5d593ddaf5c6a6e05204
Huu Bang Doan <chocotochan@gmail.com>
2026-05-04 09:40:37 +0700

    docs: relocate class and graphic

--- Mainl.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.SwingUtilities;
-public class Mainl {
-    public static void main (String[] args){
-        SwingUtilities.invokeLater(()->{
-            GameFrame game = new GameFrame();
-            game.setVisible(true);
-        });
-    }
-}
