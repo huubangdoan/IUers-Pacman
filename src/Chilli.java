24626404e5469f1d4031ac49cb590fce6628edbf
tranghuyen1409-eng <tranghuyen1409@gmail.com>
2026-05-05 15:25:26 +0700

    chilli

--- src/Chilli.java
+++ src/Chilli.java
@@ -2,10 +2,10 @@ public class Chilli extends Fruit{
     public Chilli(int x, int y, String name){
         super(x, y, "Chilli");
     }
-    
     @Override
     public void onCollected(PacMan player){
-                player.setHasThorns(true); 
-                //tạo thêm một method nữa trong con pacman là setHasThorn xong rồi cài logic vô map nhé  
+                player.setChilliMode(true);
+                player.addScore(-50);
+                player.activateChilliPower(5000); 
             }
         }
