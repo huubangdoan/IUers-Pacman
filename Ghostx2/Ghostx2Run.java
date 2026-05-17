4b08436ae02a7ad3e18ea8ba324f740be99e5be0
tranghuyen1409-eng <tranghuyen1409@gmail.com>
2026-05-17 22:47:58 +0700

    feats: update code ghostx2

--- Ghostx2/Ghostx2Run.java
+++ Ghostx2/Ghostx2Run.java
@@ -1,9 +1,6 @@
 
 public class Ghostx2Run {
-
-    public static void main(String[] args) {
-        System.out.println("Kích hoạt hệ thống Pacman...");
-        System.out.println("Chế độ nhân đôi Ghost (X2 Ghost) đã sẵn sàng.");
+    public static void main(String[] args) { 
         
         // Gửi lệnh liên kết trực tiếp sang hệ thống chạy game lõi
         GameRunner.main(args);
