ce330b26370882a290928610d5fa4727c2b1cb94
pchauityu <pchau.ityu@gmail.com>
2026-05-18 02:16:38 +0700

    feats: update setting

--- src/controller/SettingsController.java
+++ src/controller/SettingsController.java
@@ -24,7 +24,7 @@ public class SettingsController implements ActionListener {
                 cardLayout.show(mainContainer, "MainMenu"); 
                 break;
             case "Sound":
-                utils.GameConfig.isSoundOn = !utils.GameConfig.isSoundOn; 
+                /* utils.GameConfig.isSoundOn = !utils.GameConfig.isSoundOn; 
                 if (utils.GameConfig.isSoundOn) {
                     SoundManager.playBGM("src/assets/Sound/StartingBGM.wav");
                     System.out.println("Trang thai: DA MO AM THANH");
@@ -33,7 +33,7 @@ public class SettingsController implements ActionListener {
                     SoundManager.stopBGM();
                     System.out.println("Trang thai: DA TAT AM THANH");
                 // Ghi chú: thêm lệnh đổi hình cái nút thành 'Loa bị gạch chéo' ở đây
-            }
+            }*/
                 break;
             default:
                 System.out.println("chua set tinh nang cho nut nay");
--- src/view/SettingsPanel.java
+++ src/view/SettingsPanel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import utils.UIUtils;
 
 public class SettingsPanel extends JPanel {
-    private TutorialOverlay tutorialOverlay;
+    private JLabel infoPopup;
     public SettingsPanel(SettingsController settingscontroller) {
         setLayout(null); 
 
@@ -18,6 +18,26 @@ public class SettingsPanel extends JPanel {
         UIUtils.setupZoomEffect(back, backIcon, 105, 60);
         back.setActionCommand("Back");
         back.addActionListener(settingscontroller);
+
+        ImageIcon originalAboutus = new ImageIcon("src/assets/Menu Graphics/aboutus.png");
+        Image scaledAboutusImg = originalAboutus.getImage().getScaledInstance(153, 90, Image.SCALE_SMOOTH);
+        ImageIcon aboutusIcon = new ImageIcon(scaledAboutusImg);
+        JButton aboutus = new JButton(aboutusIcon);
+        aboutus.setBounds(260, 290, 153, 90);
+        UIUtils.makeButtonTransparent(aboutus);
+        UIUtils.setupZoomEffect(aboutus, aboutusIcon, 153, 90);
+        aboutus.setActionCommand("About Us");
+        aboutus.addActionListener(settingscontroller);
+
+        ImageIcon originalHelp = new ImageIcon("src/assets/Menu Graphics/help.png");
+        Image scaledHelpImg = originalHelp.getImage().getScaledInstance(153, 90, Image.SCALE_SMOOTH);
+        ImageIcon helpIcon = new ImageIcon(scaledHelpImg);
+        JButton help = new JButton(helpIcon);
+        help.setBounds(260, 375, 153, 90);
+        UIUtils.makeButtonTransparent(help);
+        UIUtils.setupZoomEffect(help, helpIcon, 153, 90);
+        help.setActionCommand("Help");
+        help.addActionListener(settingscontroller);
 
         ImageIcon originalS = new ImageIcon("src/assets/Menu Graphics/s.png");
         Image scaledSImg = originalS.getImage().getScaledInstance(105, 60, Image.SCALE_SMOOTH);
@@ -37,17 +57,31 @@ public class SettingsPanel extends JPanel {
         UIUtils.makeButtonTransparent(tutorial);
         UIUtils.setupZoomEffect(tutorial, tutorialIcon, 105, 60);
         tutorial.addActionListener(e -> {
-            tutorialOverlay.setVisible(true);
+            infoPopup.setVisible(true);
         });
 
-        ImageIcon canvaImg = new ImageIcon("src/assets/Menu Graphics/guide_canva.png");
-        tutorialOverlay = new TutorialOverlay(canvaImg);
-        tutorialOverlay.setBounds(86, 86, 500, 500); 
-        tutorialOverlay.setVisible(false);
-        
-        add(tutorialOverlay, 0);
+        ImageIcon canvaImg = new ImageIcon("src/assets/Menu Graphics/guide.png");
+        infoPopup = new JLabel(canvaImg); 
+        int overlayW = 500; 
+        int overlayH = 500; 
+        int centerX = (672 - overlayW) / 2;
+        int centerY = (672 - overlayH) / 2;
+
+        infoPopup.setBounds(centerX, centerY, overlayW, overlayH);
+        infoPopup.setVisible(false); 
+
+        infoPopup.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                infoPopup.setVisible(false);
+            }
+        });
+
+        add(infoPopup, 0);
         add(s);
         add(back);
+        add(aboutus);
+        add(help);
         add(tutorial);
 
         ImageIcon bgIcon = new ImageIcon("src/assets/Menu Graphics/setbgr1.png");
