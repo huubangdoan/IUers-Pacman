e6bbeaa638f0a0a08ff96f6c186025db26fa4a59
pchauityu <pchau.ityu@gmail.com>
2026-05-23 19:27:58 +0700

    feats: update settingpanel + kiwi + gameconfig

--- src/game/Kiwi.java
+++ src/game/Kiwi.java
@@ -2,11 +2,12 @@ package game;
 import java.awt.*;
 public class Kiwi extends Fruit{
     public Kiwi(int x, int y, String name){
-        super(x, y, name);
+        super(x, y, "Kiwi");
     }
     @Override
     public void onCollected(PacMan player){
                 player.activateKiwiDisguise();
+                player.addScore(150);
             }
             @Override
     public void draw(Graphics2D g2d, GameAssets assets) {
---
+++ src/utils/GameConfig.java
@@ -0,0 +1,5 @@
+package utils;
+public class GameConfig {
+    public static boolean isSoundOn = true;
+    
+}
